5c0fa7a8147e69cdd5ea8aa159be810f58bc1cbf
geetajeeru <geethajeeru@gmail.com>
2020-10-26 11:09:12 +0530

    Updates

---
+++ precache-manifest.c9da04d255f6ebd1247568c35aef10ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0866158209e2e0c36c2566d79bb6e0e8",
+    "url": "/geetajeeru/redux-saga/index.html"
+  },
+  {
+    "revision": "413e80ca303f7c00a6f5",
+    "url": "/geetajeeru/redux-saga/static/css/main.cd245e06.chunk.css"
+  },
+  {
+    "revision": "95afaa94fff5906c8f50",
+    "url": "/geetajeeru/redux-saga/static/js/2.2e6ef80f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/geetajeeru/redux-saga/static/js/2.2e6ef80f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413e80ca303f7c00a6f5",
+    "url": "/geetajeeru/redux-saga/static/js/main.672a9bcd.chunk.js"
+  },
+  {
+    "revision": "6326c63c1307e01ac812",
+    "url": "/geetajeeru/redux-saga/static/js/runtime-main.94575cee.js"
+  }
+]);
